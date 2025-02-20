d548ce9fec6bd0a3587e824fbfce858b3d76140f
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-02-20 19:51:10 +0500

    20.02.2025 fix: direcrions dto

--- src/dto/direction.py
+++ src/dto/direction.py
@@ -6,12 +6,10 @@ from pydantic import BaseModel
 class DirectionDTO(BaseModel):
     name: str
     description: str
-    link: str
 
     @classmethod
     def from_metadata(cls, metadata: Mapping) -> "DirectionDTO":
         return cls(
             name=metadata.get("name"),
-            description=metadata.get("description"),
-            link=metadata.get("link")
+            description=metadata.get("description")
         )
