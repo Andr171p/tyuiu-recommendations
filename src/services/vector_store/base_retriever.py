f6913742f314cfcf362e34464a6e2e42e6b9d5d0
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-02-22 01:17:19 +0500

    22.02.2025 fix: chroma_retriever

--- src/services/vector_store/base_retriever.py
+++ src/services/vector_store/base_retriever.py
@@ -1,15 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Sequence
 from numpy import ndarray
-
-from src.dto import DirectionDTO
 
 
 class BaseRetrieverService(ABC):
     @abstractmethod
-    def find_similar_directions(
+    def find_similar(
             self,
             vector: ndarray[float],
             top_n: int = 10
-    ) -> Sequence[DirectionDTO]:
+    ) -> dict:
         raise NotImplemented
--- src/services/vector_store/chroma_retriever.py
+++ src/services/vector_store/chroma_retriever.py
@@ -1,9 +1,6 @@
-from typing import Set
-
 from numpy import ndarray
 from chromadb import ClientAPI
 
-from src.dto import DirectionDTO
 from src.services.vector_store.base_retriever import BaseRetrieverService
 
 
@@ -15,17 +12,13 @@ class ChromaRetrieverService(BaseRetrieverService):
     ) -> None:
         self._collection = client_api.get_collection(collection_name)
 
-    def find_similar_directions(
+    def find_similar(
             self,
             vector: ndarray[float],
             top_n: int = 10
-    ) -> Set[DirectionDTO]:
+    ) -> dict:
         results = self._collection.query(
             query_embeddings=[vector.tolist()],
             n_results=top_n
         )
-        metadatas = results.get("metadatas")
-        return {
-            DirectionDTO.from_metadata(metadata)
-            for metadata in metadatas[0]
-        }
+        return results
