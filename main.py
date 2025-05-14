5062f7a4c2cb6461f8390b2f0e1d831155a230b0
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-14 17:40:58 +0500

    14.05.2025 fix app entry point

--- main.py
+++ main.py
@@ -5,9 +5,4 @@ from src.tyuiu_recommendations.api.app import create_fastapi_app
 
 logging.basicConfig(level=logging.INFO)
 
-# app = create_fastapi_app()
-
-import requests
-
-r = requests.get("https://192.168.16.222:8800")
-print(r)
+app = create_fastapi_app()
