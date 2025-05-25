caf03e61e93b0f8a4204ef7b751d040b1dcba883
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-25 18:19:33 +0500

    25.05.2025 fix database session.py

--- src/tyuiu_recommendations/database/session.py
+++ src/tyuiu_recommendations/database/session.py
@@ -15,20 +15,3 @@ def create_sessionmaker(pg_settings: PostgresSettings) -> async_sessionmaker[Asy
         autoflush=False,
         expire_on_commit=False
     )
-
-
-sessionmaker = create_sessionmaker(PostgresSettings())
-
-from src.tyuiu_recommendations.database.repositories import DirectionRepository
-
-repository = DirectionRepository(sessionmaker)
-
-import asyncio
-
-
-async def main() -> None:
-    direction = await repository.read(8)
-    print(direction)
-
-
-asyncio.run(main())
